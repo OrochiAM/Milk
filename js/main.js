db7e7a16123e481c459c7aa0ee499399282c2b34
Aleksa Milic <32849778+OrochiAM@users.noreply.github.com>
2026-02-25 00:55:23 +0100

    MILK.

--- js/main.js
+++ js/main.js
@@ -1,1 +1,40 @@
-const milk = document.querySelector('.milk');
+const milkButton = document.querySelector('button');
+
+const tl = gsap.timeline({ paused: true });
+
+tl.to('.milk', {
+  y: '-=125vh',
+  duration: 5,
+})
+  .to(
+    '.milk-wave',
+    {
+      xPercent: -50,
+      duration: 2.5,
+      ease: 'none',
+    },
+    '<',
+  )
+  .set('.milk-wave', {
+    xPercent: 0,
+  })
+  .to('.milk-wave', {
+    xPercent: -50,
+    duration: 2.5,
+    ease: 'none',
+  });
+
+const playSounds = () => {
+  const moo = new Audio('audio/cow-moo.mp3');
+  moo.play();
+
+  const pour = new Audio('audio/pour.mp3');
+  pour.play();
+};
+
+const cowHandle = () => {
+  playSounds();
+  tl.restart();
+};
+
+milkButton.addEventListener('click', cowHandle);
